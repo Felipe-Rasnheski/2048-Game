4e5f3046c9f18a170bc4aa1940737e0f74d2236e
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-07-07 07:40:48 -0400

    class

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ import Grid from "./Grid.js"
 const gameBoard = document.getElementById("tabuleiro-jogo")
 
 const grid = new Grid(gameBoard)
+
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
