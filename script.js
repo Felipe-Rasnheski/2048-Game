abf1ac76cf5c4aaa677cb444cc3baa56838715b6
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-07-07 16:09:11 -0400

    func key add

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ const grid = new Grid(gameBoard)
 
 grid.randomEmptyCell().tile = new Tile(gameBoard)
 grid.randomEmptyCell().tile = new Tile(gameBoard)
+setInput()
+
+function setInput() {
+  window.addEventListener("keydown", handleInput, { once: true})
+}
+
+function handleInput(e) {
+  console.log(e.key)
+  switch(e.key) {
+    case "ArrowUp":
+      moveUp()
+      break
+    case "ArrowDown":
+      moveDown()
+      break
+    case "ArrowLeft":
+      moveLeft()
+      break
+    case "ArrowRight":
+      moveRight()
+      break
+    default: 
+      setInput()
+      return
+  }
+  setInput()
+}
+
