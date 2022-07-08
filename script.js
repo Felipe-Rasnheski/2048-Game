d66207dda0c9b82a06570afc5cd7c804f3266258
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-07-08 07:59:22 -0400

    move

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function setInput() {
 }
 
 function handleInput(e) {
-  console.log(e.key)
   switch(e.key) {
     case "ArrowUp":
       moveUp()
@@ -34,3 +33,29 @@ function handleInput(e) {
   setInput()
 }
 
+function moveUp() {
+  return slideTiles(grid.cellsByColumn)
+}
+
+function slideTiles(cells) {
+  cells.forEach(group => {
+    for(let i = 1; i < group.length; i++) {
+      const cell = group[i]
+      if(cell.tile == null) continue
+      let lastValidCell
+      for(let j = i - 1; j >= 0; j--) {
+        const moveToCell = group[j]
+        if(!moveToCell.canAccept(cell.tile)) break 
+        lastValidCell = moveToCell
+      }
+      if(lastValidCell != null) {
+        if(lastValidCell.tile != null) {
+          lastValidCell.mergeTile = cell.tile
+        } else {
+          lastValidCell.tile = cell.tile
+        }
+        cell.tile = null
+      }
+    }
+  })
+}
